90dc7d2ae72a5843fded79f080a1a7d03edf7a8f
topstar210 <happycamper0210@gmail.com>
2024-08-05 23:53:38 +0300

    chore: add touch event on tabsection

--- src/components/TabSection/index.tsx
+++ src/components/TabSection/index.tsx
@@ -5,11 +5,33 @@ import heroCharacter from "@/assets/imgs/heroCharacter.png";
 interface TabSectionProps {}
 
 const TabSection = ({ ...props }: TabSectionProps) => {
+  const handleTabClick = (e: any) => {
+    const parent = e.currentTarget;
+    const rect = parent.getBoundingClientRect();
+    const x = e.clientX - rect.left - 8;
+    const y = e.clientY - rect.top - 8;
+
+    const plusOne = document.createElement("div");
+    plusOne.className = "plus-one";
+    plusOne.textContent = "+1";
+    plusOne.style.left = `${x}px`;
+    plusOne.style.top = `${y}px`;
+
+    const grandparent = parent.parentElement;
+    grandparent.appendChild(plusOne);
+
+    plusOne.addEventListener("animationend", () => {
+      plusOne.remove();
+    });
+  };
+
   return (
     <div className="app-tabsection" {...props}>
-      <div className="app-tab-pan">
-        <div className="app-tab-pan-in">
-          <img src={heroCharacter} alt="" />
+      <div style={{ position: "relative" }}>
+        <div className="app-tab-pan" onClick={handleTabClick} onTouchEnd={handleTabClick}>
+          <div className="app-tab-pan-in">
+            <img src={heroCharacter} alt="" />
+          </div>
         </div>
       </div>
     </div>
