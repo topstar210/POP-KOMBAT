73d302bc95379b6fbc4cafc5d9ec15b694b73f4a
topstar210 <happycamper0210@gmail.com>
2024-08-05 19:54:37 +0300

    ci: add initData in useApp

--- src/providers/useApp.tsx
+++ src/providers/useApp.tsx
@@ -1,25 +1,48 @@
-import { ReactNode, createContext, useContext, useState, useEffect } from 'react';
+import {
+  ReactNode,
+  createContext,
+  useContext,
+} from "react";
+import {
+  useInitData,
+  useLaunchParams,
+  type InitDataParsed,
+} from "@telegram-apps/sdk-react";
+import { Placeholder } from "@telegram-apps/telegram-ui";
 
 interface AppContextType {
-  
+  initData: InitDataParsed;
 }
 
 const AppContext = createContext<AppContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  
+  const initDataRaw = useLaunchParams().initDataRaw;
+  const initData = useInitData();
 
+  if (!initData || !initDataRaw) {
+    return (
+      <Placeholder
+        header="Oops"
+        description="Application was launched with missing init data"
+      >
+        <img
+          alt="Telegram sticker"
+          src="https://xelene.me/telegram.gif"
+          style={{ display: "block", width: "144px", height: "144px" }}
+        />
+      </Placeholder>
+    );
+  }
   return (
-    <AppContext.Provider value={{  }}>
-      {children}
-    </AppContext.Provider>
+    <AppContext.Provider value={{ initData }}>{children}</AppContext.Provider>
   );
 };
 
 export const useApp = (): AppContextType => {
   const context = useContext(AppContext);
   if (context === undefined) {
-    throw new Error('useApp must be used within an AppProvider');
+    throw new Error("useApp must be used within an AppProvider");
   }
   return context;
 };
