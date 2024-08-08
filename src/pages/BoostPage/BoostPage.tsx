22f8b48027d2e78b79fc49dca1190f6cef1aa55c
topstar210 <happycamper0210@gmail.com>
2024-08-09 00:35:35 +0300

    style: fix color issue on mobile

--- src/pages/BoostPage/BoostPage.tsx
+++ src/pages/BoostPage/BoostPage.tsx
@@ -12,7 +12,7 @@ const BoostPage = () => {
   return (
     <div className="main-page">
       <Box className="bp-header">
-        <h1 className="bp-title">Your Balance</h1>
+        <h1 className="bp-title fade-down">Your Balance</h1>
         <div className="bp-mybalance">
           <MyBalance className="fade-in" value={538507456} />
         </div>
