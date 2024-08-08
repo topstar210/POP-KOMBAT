2c47e9d9593f74e65bc0799f267ab2257e3588a5
topstar210 <happycamper0210@gmail.com>
2024-08-09 00:25:54 +0300

    feat: add earn page

--- src/components/Earn/Item.tsx
+++ src/components/Earn/Item.tsx
@@ -1,13 +1,46 @@
 import "./EarnItem.css";
+import { formatToFixed } from "@/utilities/number";
+
+import tokenIcon from "@/assets/icons/token.png";
+import arrowRightIcon from "@/assets/icons/arrow-right.png";
+import checkIcon from "@/assets/icons/check-circle.png";
 
 interface EarnItemProps {
+  icon: any;
+  title: string;
+  coin: number;
+  isChecked: boolean;
+
   className?: string;
+  onClick?: () => void;
 }
 
-const EarnItem = ({ className, ...props }: EarnItemProps) => {
+const EarnItem = ({
+  icon,
+  title,
+  coin,
+  isChecked,
+  className,
+  onClick,
+  ...props
+}: EarnItemProps) => {
   return (
-    <div className={`app-earnitem ${className}`} {...props}>
-      
+    <div className={`app-earnitem ${className}`} {...props} onClick={onClick}>
+      <img src={icon} alt="" width={32} height={32} />
+      <div className="item-earn-info">
+        <div className="title">{title}</div>
+        <div className="coin">
+          <img src={tokenIcon} alt="T" />
+          &nbsp;
+          <span> +{formatToFixed(coin)}</span>
+        </div>
+      </div>
+      <img
+        src={isChecked ? checkIcon : arrowRightIcon}
+        alt=""
+        width={24}
+        height={24}
+      />
     </div>
   );
 };
--- src/pages/EarnPage/EarnPage.tsx
+++ src/pages/EarnPage/EarnPage.tsx
@@ -1,9 +1,90 @@
 import "./EarnPage.css";
+import { Box } from "@/components/system";
+import EarnItem from "@/components/Earn/Item";
+
+import tokenimg from "@/assets/imgs/token-image.png";
+
+import spotifyIcon from "@/assets/icons/spotify.png";
+import calandarIcon from "@/assets/icons/calandar.png";
+import xIcon from "@/assets/icons/x.png";
+import telegramIcon from "@/assets/icons/telegram.png";
+import binanceIcon from "@/assets/icons/binance.png";
+import inviteIcon from "@/assets/icons/invite.png";
 
 const EarnPage = () => {
   return (
-    <div className="main-page">
-      
+    <div className="main-page earn-page">
+      <Box className="display-section">
+        <div className="display-section-cover">
+          <div className="er-header">
+            <img src={tokenimg} alt="" width={"50%"} />
+            <h1>Earn More Coins</h1>
+          </div>
+          <div className="er-list-cointainer">
+            <h2>Spotify STreaming</h2>
+            <div className="er-list">
+              <EarnItem
+                icon={spotifyIcon}
+                title="Phasellus mauris volutpat"
+                coin={308880}
+                isChecked={false}
+              />
+              <EarnItem
+                icon={spotifyIcon}
+                title="Phasellus mauris volutpat"
+                coin={308880}
+                isChecked={true}
+              />
+            </div>
+          </div>
+          <div className="er-list-cointainer">
+            <h2>Daily Tasks</h2>
+            <div className="er-list">
+              <EarnItem
+                icon={calandarIcon}
+                title="Phasellus mauris volutpat"
+                coin={308880}
+                isChecked={false}
+              />
+            </div>
+          </div>
+          <div className="er-list-cointainer">
+            <h2>Tasks List</h2>
+            <div className="er-list">
+              <EarnItem
+                icon={telegramIcon}
+                title="Join our TG Channel"
+                coin={308880}
+                isChecked={false}
+              />
+              <EarnItem
+                icon={xIcon}
+                title="Follow Producers on X"
+                coin={308880}
+                isChecked={false}
+              />
+              <EarnItem
+                icon={binanceIcon}
+                title="Sign up to Binance"
+                coin={308880}
+                isChecked={false}
+              />
+              <EarnItem
+                icon={inviteIcon}
+                title="Invite a Friend"
+                coin={308880}
+                isChecked={false}
+              />
+              <EarnItem
+                icon={inviteIcon}
+                title="Invite 3 Friends"
+                coin={308880}
+                isChecked={false}
+              />
+            </div>
+          </div>
+        </div>
+      </Box>
     </div>
   );
 };
