4560483981c379bfa0f528b93f99d5b97c684b4a
topstar210 <happycamper0210@gmail.com>
2024-09-11 00:34:05 +0300

    chore: update bootpage icons

--- src/pages/BoostPage/BoostPage.tsx
+++ src/pages/BoostPage/BoostPage.tsx
@@ -4,7 +4,9 @@ import { Box } from "@/components/system";
 import { MyBalance } from "@/components";
 import { formatToFixed } from "@/utilities/number";
 
-import circleIcon from "@/assets/icons/boost-circle.png";
+import energyIcon from "@/assets/icons/energyIcon.png";
+import arrowFigerUpIcon from "@/assets/icons/arrowFigerUp.png";
+import lightningIcon from "@/assets/icons/lightning.png";
 import tokenIcon from "@/assets/icons/token.png";
 import arrowRightIcon from "@/assets/icons/arrow-right.png";
 
@@ -25,9 +27,9 @@ const BoostPage = () => {
         <h2>Free Daily Boosterss</h2>
         <div className="bp-daily-booster-list">
           <div className="app-boostitem">
-            <img src={circleIcon} alt="" width={32} height={32} />
+            <img src={energyIcon} alt="" width={32} height={32} />
             <div className="app-boostitem-info">
-              <div className="title">Lorem Ipsum sit Dolor</div>
+              <div className="title">Energy Refill</div>
               <div className="description">6/6 Available</div>
             </div>
             <img src={arrowRightIcon} alt="" width={24} height={24} />
@@ -38,7 +40,7 @@ const BoostPage = () => {
         <h2>Boosterss</h2>
         <div className="bp-booster-list">
           <div className="app-boostitem" style={{ height: 56 }}>
-            <img src={circleIcon} alt="" width={32} height={32} />
+            <img src={arrowFigerUpIcon} alt="" width={32} height={32} />
             <div className="app-boosters">
               <div>
                 <div className="title">Multitap</div>
@@ -53,7 +55,7 @@ const BoostPage = () => {
             <img src={arrowRightIcon} alt="" width={24} height={24} />
           </div>
           <div className="app-boostitem" style={{ height: 56 }}>
-            <img src={circleIcon} alt="" width={32} height={32} />
+            <img src={lightningIcon} alt="" width={32} height={32} />
             <div className="app-boosters">
               <div>
                 <div className="title">Energy Limit</div>
