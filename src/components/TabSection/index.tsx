e4a19d002b99b06a4e9839831097b60db9596173
topstar210 <happycamper0210@gmail.com>
2024-09-11 01:16:49 +0300

    feat: multi tab

--- src/components/TabSection/index.tsx
+++ src/components/TabSection/index.tsx
@@ -17,52 +17,59 @@ const TabSection = ({
   className,
   ...props
 }: TabSectionProps) => {
-  const handleTabClick = (e: any) => {
-    setBalance({
-      balance: gameData.balance + 1,
-      totalEarning: gameData.totalEarning + 1,
-    });
-
-    setDecrementCurEnergy();
-
-    /**
-     * animation section start -----------------------------------------------------
-     */
+  const handleTouch = (e: any) => {
     const parent = e.currentTarget;
     const rect = parent.getBoundingClientRect();
-    const x = e.clientX - rect.left - 8;
-    const y = e.clientY - rect.top - 8;
 
-    const plusOne = document.createElement("div");
-    plusOne.className = "plus-one";
-    plusOne.textContent = "+1";
-    plusOne.style.left = `${x}px`;
-    plusOne.style.top = `${y}px`;
+    if (e.touches.length > 5) return;
 
-    const grandparent = parent.parentElement;
-    grandparent.appendChild(plusOne);
+    // Loop through each touch point (multi-touch)
+    for (let i = 0; i < e.touches.length; i++) {
+      setBalance({
+        balance: gameData.balance + 1,
+        totalEarning: gameData.totalEarning + 1,
+      });
 
-    // Animate the .app-tab-pan
-    const appTabPan = parent;
-    const rotateX = ((y - rect.height / 2) / rect.height) * 30; // Adjust the factor to control the rotation
-    const rotateY = ((x - rect.width / 2) / rect.width) * -30; // Adjust the factor to control the rotation
-    appTabPan.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+      setDecrementCurEnergy();
 
-    // Reset the animation
-    setTimeout(() => {
-      appTabPan.style.transform = "rotateX(0deg) rotateY(0deg)";
-    }, 150);
+      /**
+       * animation section start -----------------------------------------------------
+       */
+      const touch = e.touches[i];
+      const x = touch.clientX - rect.left - 8;
+      const y = touch.clientY - rect.top - 8;
 
-    plusOne.addEventListener("animationend", () => {
-      plusOne.remove();
-    });
-    // animation section end -----------------------------------------------------
+      const plusOne = document.createElement("div");
+      plusOne.className = "plus-one";
+      plusOne.textContent = "+1";
+      plusOne.style.left = `${x}px`;
+      plusOne.style.top = `${y}px`;
+
+      const grandparent = parent.parentElement;
+      grandparent.appendChild(plusOne);
+
+      // Animate the .app-tab-pan
+      const appTabPan = parent;
+      const rotateX = ((y - rect.height / 2) / rect.height) * 30; // Adjust the factor to control the rotation
+      const rotateY = ((x - rect.width / 2) / rect.width) * -30; // Adjust the factor to control the rotation
+      appTabPan.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+
+      // Reset the animation
+      setTimeout(() => {
+        appTabPan.style.transform = "rotateX(0deg) rotateY(0deg)";
+      }, 150);
+
+      plusOne.addEventListener("animationend", () => {
+        plusOne.remove();
+      });
+      // animation section end -----------------------------------------------------
+    }
   };
 
   return (
     <div className={`app-tabsection ${className}`} {...props}>
       <div style={{ position: "relative" }}>
-        <div className="app-tab-pan" onClick={handleTabClick}>
+        <div className="app-tab-pan" onTouchStart={handleTouch}>
           <div className="app-tab-pan-in">
             <img src={heroCharacter} alt="" />
           </div>
