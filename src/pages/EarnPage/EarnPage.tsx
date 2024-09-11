7d83993d897ada05e1b186db48cc73c84b2289bc
topstar210 <happycamper0210@gmail.com>
2024-09-12 00:20:09 +0300

    feat: create daily reward modal

--- src/pages/EarnPage/EarnPage.tsx
+++ src/pages/EarnPage/EarnPage.tsx
@@ -1,15 +1,16 @@
 import "./EarnPage.css";
 import { Box } from "@/components/system";
 import EarnItem from "@/components/Earn/Item";
+import { Modal } from "@/components/system";
 
 import tokenimg from "@/assets/imgs/token-image.png";
-
 import spotifyIcon from "@/assets/icons/spotify.png";
 import calandarIcon from "@/assets/icons/calandar.png";
 import xIcon from "@/assets/icons/x.png";
 import telegramIcon from "@/assets/icons/telegram.png";
 import binanceIcon from "@/assets/icons/binance.png";
 import inviteIcon from "@/assets/icons/invite.png";
+import dailyReward from "@/assets/imgs/modal/daily_reward.png";
 
 const EarnPage = () => {
   return (
@@ -43,7 +44,7 @@ const EarnPage = () => {
               <EarnItem
                 icon={calandarIcon}
                 title="Daily Reward"
-                coin={308880} 
+                coin={308880}
                 isChecked={false}
               />
             </div>
@@ -85,6 +86,72 @@ const EarnPage = () => {
           </div>
         </div>
       </Box>
+      <Modal isOpen={false} onClose={() => {}}>
+        <div className="app-dailyreward-mcontainer">
+          <img
+            src={dailyReward}
+            alt="daily-img"
+            className="daily-img"
+            width={103}
+            height={103}
+          />
+          <h2>Daily Reward</h2>
+          <p>Earn bonus coins for logging in daily without missing a beat</p>
+          <div className="dailyreward-cards">
+            <div className="dailyreward-card actived">
+              <div>day 1</div>
+              <img src={tokenimg} alt="" width={36} />
+              <div>500</div>
+            </div>
+            <div className="dailyreward-card available">
+              <div>day 2</div>
+              <img src={tokenimg} alt="" width={36} />
+              <div>1k</div>
+            </div>
+            <div className="dailyreward-card">
+              <div>day 3</div>
+              <img src={tokenimg} alt="" width={36} />
+              <div>2.5k</div>
+            </div>
+            <div className="dailyreward-card">
+              <div>day 4</div>
+              <img src={tokenimg} alt="" width={36} />
+              <div>5k</div>
+            </div>
+            <div className="dailyreward-card">
+              <div>day 5</div>
+              <img src={tokenimg} alt="" width={36} />
+              <div>15k</div>
+            </div>
+            <div className="dailyreward-card">
+              <div>day 6</div>
+              <img src={tokenimg} alt="" width={36} />
+              <div>25k</div>
+            </div>
+            <div className="dailyreward-card">
+              <div>day 7</div>
+              <img src={tokenimg} alt="" width={36} />
+              <div>100k</div>
+            </div>
+            <div className="dailyreward-card">
+              <div>day 8</div>
+              <img src={tokenimg} alt="" width={36} />
+              <div>500k</div>
+            </div>
+            <div className="dailyreward-card">
+              <div>day 9</div>
+              <img src={tokenimg} alt="" width={36} />
+              <div>1M</div>
+            </div>
+            <div className="dailyreward-card">
+              <div>day 10</div>
+              <img src={tokenimg} alt="" width={36} />
+              <div>5M</div>
+            </div>
+          </div>
+          <button onClick={() => {}}>Claim</button>
+        </div>
+      </Modal>
     </div>
   );
 };
