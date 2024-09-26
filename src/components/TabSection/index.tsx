e69248b5886548e038c4f791f22b3af1d5882170
topstar210 <happycamper0210@gmail.com>
2024-09-26 17:47:19 +0300

    tap touch event when touch end

--- src/components/TabSection/index.tsx
+++ src/components/TabSection/index.tsx
@@ -23,13 +23,64 @@ const TabSection = ({
 
   const handleTouch = (e: any) => {
     setOnPress(true);
+    // const parent = e.currentTarget;
+    // const rect = parent.getBoundingClientRect();
+
+    // if (e.touches.length > 5) return;
+
+    // // Loop through each touch point (multi-touch)
+    // for (let i = 0; i < e.touches.length; i++) {
+    //   setBalance({
+    //     balance: gameData.balance + 1,
+    //     totalEarning: gameData.totalEarning + 1,
+    //   });
+
+    //   setDecrementCurEnergy();
+
+    //   /**
+    //    * animation section start -----------------------------------------------------
+    //    */
+    //   const touch = e.touches[i];
+    //   const x = touch.clientX - rect.left - 8;
+    //   const y = touch.clientY - rect.top - 8;
+
+    //   const plusOne = document.createElement("div");
+    //   plusOne.className = "plus-one";
+    //   plusOne.textContent = "+1";
+    //   plusOne.style.left = `${x}px`;
+    //   plusOne.style.top = `${y}px`;
+
+    //   const grandparent = parent.parentElement;
+    //   grandparent.appendChild(plusOne);
+
+    //   plusOne.addEventListener("animationend", () => {
+    //     plusOne.remove();
+    //   });
+
+    //   // Animate the .app-tab-pan
+    //   const appTabPan = parent;
+    //   const rotateX = ((y - rect.height / 2) / rect.height) * 30; // Adjust the factor to control the rotation
+    //   const rotateY = ((x - rect.width / 2) / rect.width) * -30; // Adjust the factor to control the rotation
+    //   appTabPan.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+
+    //   // Reset the animation
+    //   setTimeout(() => {
+    //     appTabPan.style.transform = "rotateX(0deg) rotateY(0deg)";
+    //   }, 150);
+    //   // animation section end -----------------------------------------------------
+    // }
+  };
+
+  const handleTouchEnd = (e:any) => {
+    setOnPress(false);
     const parent = e.currentTarget;
     const rect = parent.getBoundingClientRect();
 
-    if (e.touches.length > 5) return;
+
+    if (e.changedTouches.length > 5) return;
 
     // Loop through each touch point (multi-touch)
-    for (let i = 0; i < e.touches.length; i++) {
+    for (let i = 0; i < e.changedTouches.length; i++) {
       setBalance({
         balance: gameData.balance + 1,
         totalEarning: gameData.totalEarning + 1,
@@ -40,7 +91,7 @@ const TabSection = ({
       /**
        * animation section start -----------------------------------------------------
        */
-      const touch = e.touches[i];
+      const touch = e.changedTouches[i];
       const x = touch.clientX - rect.left - 8;
       const y = touch.clientY - rect.top - 8;
 
@@ -69,10 +120,6 @@ const TabSection = ({
       }, 150);
       // animation section end -----------------------------------------------------
     }
-  };
-
-  const handleTouchEnd = () => {
-    setOnPress(false);
   };
 
   return (
