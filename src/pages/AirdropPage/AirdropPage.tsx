2a3725161c94fc0b774ffa6006b0d853d51ae222
topstar210 <happycamper0210@gmail.com>
2024-09-28 21:05:41 +0300

    producer not appearing from behind menu

--- src/pages/AirdropPage/AirdropPage.tsx
+++ src/pages/AirdropPage/AirdropPage.tsx
@@ -1,8 +1,39 @@
+import "./AirdropPage.css"
+import { useEffect, useState } from "react";
+
 import ComingSoon from "@/components/ComingSoon";
+import producerImg from "@/assets/imgs/producer.png";
 
 const AirdropPage = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    // Initially make the div appear with rise animation after a short delay
+    const timer = setTimeout(() => {
+      setIsVisible(true);
+    }, 100);
+
+    return () => clearTimeout(timer); // Cleanup the timer on unmount
+  }, []);
+
+  const handleClick = () => {
+    // Hide the div when clicked
+    setIsVisible(false);
+
+    // Reappear the div after 5 seconds
+    setTimeout(() => {
+      setIsVisible(true);
+    }, 5000);
+  };
+
   return <div className="main-page">
     <ComingSoon />
+    <div
+      className={`animated-div ${isVisible ? "rise" : "hide"}`}
+      onClick={handleClick}
+    >
+      <img src={producerImg} alt="" />
+    </div>
   </div>;
 };
 
