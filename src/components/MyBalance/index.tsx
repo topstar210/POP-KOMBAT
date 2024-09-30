8fad576cadad6f7edcd49922e29b3b704dada6ad
topstar210 <happycamper0210@gmail.com>
2024-09-30 12:33:48 +0300

    test-6: lucky font on iphone

--- src/components/MyBalance/index.tsx
+++ src/components/MyBalance/index.tsx
@@ -13,7 +13,7 @@ const MyBalance = ({ value, className, ...props }: MyBalanceProps) => {
   return (
     <div className={`coin-counter ${className}`} {...props}>
       <img src={coinImg} alt="" width={40} height={40} />
-      <span style={{background: 'blue' }}>{coinVal}</span>
+      <span>{coinVal}</span>
     </div>
   );
 };
