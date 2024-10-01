48f8e84194a126516213c20e1b4ef8adfdf6c8b4
topstar210 <happycamper0210@gmail.com>
2024-10-01 23:16:29 +0300

    update the formatToFixed function

--- src/utilities/number.ts
+++ src/utilities/number.ts
@@ -4,18 +4,27 @@
  * @returns string like 15M, 5k ....
  */
 export const formatToFixed = (num: number) => {
+  const removeTrailingZeros = (formattedNum: string) => {
+    return formattedNum.endsWith(".00")
+      ? formattedNum.slice(0, -3)
+      : formattedNum.endsWith("0")
+      ? formattedNum.slice(0, -1)
+      : formattedNum;
+  };
+
   if (num >= 1000 && num < 1000000) {
-    return (num / 1000).toFixed(2) + "k";
+    return removeTrailingZeros((num / 1000).toFixed(2)) + "k";
   } else if (num >= 1000000 && num < 1000000000) {
-    return (num / 1000000).toFixed(2) + "M";
+    return removeTrailingZeros((num / 1000000).toFixed(2)) + "M";
   } else if (num >= 1000000000 && num < 1000000000000) {
-    return (num / 1000000000).toFixed(2) + "B";
+    return removeTrailingZeros((num / 1000000000).toFixed(2)) + "B";
   } else if (num >= 1000000000000) {
-    return (num / 1000000000000).toFixed(2) + "T";
+    return removeTrailingZeros((num / 1000000000000).toFixed(2)) + "T";
   } else {
-    return num.toFixed(3).toString();
+    return removeTrailingZeros(num.toFixed(3));
   }
 };
+
 
 /**
  *
